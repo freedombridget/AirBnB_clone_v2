e9b965ed8250dd62c2902436ff72b4fe019fd235
freedombridget <86961410+freedombridget@users.noreply.github.com>
2022-12-05 04:56:18 +0100

    reverted to earlier airbnb package

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,19 +1,13 @@
--- Creates a MySQL server with:
---   Database hbnb_test_db.
---   User hbnb_test with password hbnb_test_pwd in localhost.
---   Grants all privileges for hbnb_test on hbnb_test_db.
---   Grants SELECT privilege for hbnb_test on performance_schema.
-
+-- Creates the database hbnb_test_db with specified paramenters
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+-- Creates user if doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+-- Sets password for user
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+-- Grants privileges to user on database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- Grants select privileges to user on performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Flush privileges
 FLUSH PRIVILEGES;
